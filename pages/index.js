b0e9ead1ed2b296b0aa5c5d39a09bb52583db349
Marc Peter <35533897+MarcGordonPeter@users.noreply.github.com>
2022-09-28 09:31:24 +0200

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,133 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <head>
+        <title>.onPoint</title>
+        <meta charset="utf-8" />
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
+        <link
+          href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
+          rel="stylesheet"
+        />
+      </head>
+      <nav
+        className="flex flex-col lg:flex-row justify-center font-bold pb-16 :text-4xl justify-around p-20"
+        id="navigation"
+      >
+        <a href="#">Unsere Vision</a>
+        <a href="#">Unser Team</a>
+        <a href="#">Kontakt</a>
+      </nav>
+      <div className="p-20 bg-grey-500">
+        <h1 className="font-bold text-2xl">Unsere Vision</h1>
+        <p>
+          Wir entwickeln als Team Softwareprodukte, sodass Sie mit der besten
+          und aktuellsten Software ausgestattet werden. Unsere Entwicklung
+          arbeitet zielstrebig auf Exzellenz ihrer Wünsche hin und wird Ihnen
+          eine einfache und trotzdem zuverlässige Lösung bieten. Hier wird die
+          Software Ihrer Vorstellung Realität.
         </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
+      </div>
+      <div className="p-20 bg-grey-500">
+        <h1 className="font-bold text-2xl">Unser Team</h1>
+        <p>
+          Wir sind ein motiviertes Team von sechs Studierenden der Universität
+          zu Köln.
+        </p>
+        <div className="grid gap-10 grid- md:grid-cols-2 2xl:grid-cols-3 p-10">
+          <div className="p-20 bg-gray-100 border rounded-3xl flex flex-col justify-evenly">
+            <div className="">
+              <h1 className="font-semibold">Marc</h1>
+            </div>
+            <div className="">
+              <img
+                className="object-right object-cover"
+                src="imoji4.jpeg"
+                alt="This is a dummy picture"
+              />
+            </div>
+            <div>
+              <p>
+                Ich bin 20 Jahre alt, studiere im 5. Semester und freue mich auf
+                die kommende Zusammenarbeit
+              </p>
+            </div>
+          </div>
+          <div className="p-20 bg-gray-100 border rounded-3xl">
+            <h1 className="font-semibold">Jonas</h1>
+            <div className="">
+              <img
+                className="object-right object-cover"
+                src="imoji4.jpeg"
+                alt="This is a dummy picture"
+              />
+            </div>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Ich bin 22 Jahre alt, studiere im 5. Semester und warte gespannt
+              auf Ihre Anfrage!
             </p>
-          </a>
+          </div>
+          <div className="p-20 bg-gray-100 border rounded-3xl">
+            <h1 className="font-semibold">Lukas</h1>
+            <div className="">
+              <img
+                className="object-right object-cover"
+                src="imoji4.jpeg"
+                alt="This is a dummy picture"
+              />
+            </div>
+            <p>
+              Ich bin 22 Jahre alt, studiere im 7. Semester und freue mich auf
+              neue Kontakte.
+            </p>
+          </div>
+          <div className="p-20 bg-gray-100 border rounded-3xl">
+            <h1 className="font-semibold">Jingwen</h1>
+            <div className="">
+              <img
+                className="object-right object-cover"
+                src="imoji4.jpeg"
+                alt="This is a dummy picture"
+              />
+            </div>
+            <p>ich bin 22 Jahre alt, im 7. Semester.</p>
+          </div>
+          <div className="p-20 bg-gray-100 border rounded-3xl">
+            <h1 className="font-semibold">Felicia</h1>
+            <div className="">
+              <img
+                className="object-right object-cover"
+                src="imoji4.jpeg"
+                alt="This is a dummy picture"
+              />
+            </div>
+            <p>Beschreibung</p>
+          </div>
+          <div className="p-20 bg-gray-100 border rounded-3xl">
+            <h1 className="font-semibold">Michelle</h1>
+            <div className="">
+              <img
+                className="object-right object-cover"
+                src="imoji4.jpeg"
+                alt="This is a dummy picture"
+              />
+            </div>
+            <p>
+              Ich bin 23 Jahre alt, im 7. Semester und hoffe auf eine gute
+              Zusammenarbeit!
+            </p>
+          </div>
         </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+        <div className="p-20">
+          <h1 className="font-bold text-2xl">Kontakt</h1>
+          <p>
+            Wir bei onPoint sind immer und überall und auch an Weihnachten
+            erreichbar! Wir kennen keine Freizeit!
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
