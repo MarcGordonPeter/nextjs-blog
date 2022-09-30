769fe12eb108e28ef9e162c8b3f38c9541526beb
Marc Peter <mpeter6@smail.uni-koeln.de>
2022-09-30 14:14:52 +0200

    changed padding for mobile

--- components/content.js
+++ components/content.js
@@ -5,7 +5,7 @@ export default function Content({ title = "", children: text }) {
   return (
     <div
       data-testid={title}
-      className="border-b border-grey-500 p-5 md:p-10 bg-grey-500 border rounded-3xl text-lg md:text-2xl text-left md:text-justify"
+      className="border-b border-grey-500 p-2 md:p-10 bg-grey-500 border rounded-3xl text-lg md:text-2xl text-left md:text-justify"
     >
       <h1 id="team" className="font-mono font-bold text-4xl text-center pb-20">
         {title}
